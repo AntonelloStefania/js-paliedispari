8a69a4fb3083ac7a223007c77cc930350adad07a
AntonelloStefania <antonelloste94@gmail.com>
2023-05-25 17:08:56 +0200

    [add] contenuto esercizio palindromo

---
+++ palindromo/js/script.js
@@ -0,0 +1,29 @@
+let submit = document.getElementById('submit');
+let return_box= document.querySelector('.return-box');
+return_box.innerHTML='';
+let nome;
+
+
+    function nameCheck(name_check){
+    nome_split = nome.split('');
+    nome_reverse = nome_split.reverse();
+    full_nome_reverse = nome_reverse.join('');
+
+        if(nome===full_nome_reverse){
+            return true
+        } else{
+            return false
+        }
+        }   
+                
+submit.addEventListener('click', function(){
+    nome = document.getElementById('name').value;
+    nameCheck(nome)
+        if(nameCheck(nome)=== true){
+            return_box.innerHTML = 'la parola è palindroma'
+        } else if (!isNaN(nome)){
+            return_box.innerHTML = 'inserire una parola valida'
+        } else {
+            return_box.innerHTML = 'la parola non è palindroma'
+        }
+})
